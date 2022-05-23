a610805e9bd1451ca2b141691e7bb1da02b0cf3b
mvandendorpel <chaosengel@gmail.com>
2022-05-22 18:00:04 -0700

    Updates to OAS/Router
    update OAS doc to match current implemented endpoint
    updated API router for future endpoints(survey data and research tool)
    Added untested password update function

--- Website/nursing-survey-api/routers/api-router.mjs
+++ Website/nursing-survey-api/routers/api-router.mjs
@@ -5,9 +5,32 @@ import {registerNewUser, logInUser} from '../controllers/user-api-controller.mjs
 const router = express.Router();
 
 router.route('/users')
-.post(registerNewUser);
+.post(registerNewUser)
+//Not Yet Implemented
+//.patch(updateUserInfo);
 
 router.route('/login')
 .post(passport.authenticate('local', {session: false}), logInUser);
+//Not Yet Implemented
+//.patch(updateUserPassword);
 
+//Not Yet Implemented
+// router.route('/startsurvey')
+// .post(postStartDaySurvey)
+// .get(getStartDayData)
+// .patch(editStartDayData);
+
+
+// router.route('/endsurvey')
+// .post(postEndDaySurvey)
+// .get(getEndDayData)
+// .patch(editEndDayData);
+
+// router.route('/weeklysurvey')
+// .post(postWeeklySurvey)
+// .get(getWeeklyData)
+// .patch(updateWeeklyData)
+
+// router.route('/researcher')
+// .get(getResearchData)
 export default router;
