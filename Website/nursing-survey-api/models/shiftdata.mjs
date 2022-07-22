87baabce4c256fd5d6ece952c43b4981706e555c
TeamJCINS <chaosengel@gmail.com>
2022-07-21 20:16:16 -0700

    encrypt shiftdata

--- Website/nursing-survey-api/models/shiftdata.mjs
+++ Website/nursing-survey-api/models/shiftdata.mjs
@@ -1,11 +1,48 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import db from './index.mjs';
+import { encrypt, decrypt } from '../util.js';
 export const ShiftData = db.sequelize.define('shiftdata', {
     startTime: DataTypes.DATE,
     endTime: DataTypes.DATE,
     survey_Id: DataTypes.INTEGER,
   }, 
   {
+    hooks : {
+    
+      beforeCreate : (obj , options) => {
+          {
+              
+              obj.startTime = encrypt(obj.startTime);
+              obj.endTime = encrypt(obj.endTime);
+          }
+      },
+      afterFind : (objArray) => {
+          {
+              // Maybe you want to do something like, encrypt every string field
+              // You should be able to loop through the fields and look for the types
+
+              // Another possibility is that you inspect the objects to see what type they are
+              // and have a configuration of what fields are encrypted
+
+              // Or maybe sequelize lets you add custom attributes to fields? this might be best;
+              // then you would just encrypt every field that has that attribute set
+
+              // If you could do that, then the encrypt/decrypt, beforeCreate, afterFind implementations could be shared.
+              if(objArray.constructor === Array) {
+                  for (let i = 0; i < objArray.length; i++) {
+                      
+                      objArray[i].dataValues.startTime = decrypt(objArray[i].dataValues.startTime);
+                      objArray[i].dataValues.endTime = decrypt(objArray[i].dataValues.endTime);
+                  }
+              } else {
+                objArray.dataValues.startTime = decrypt(objArray.dataValues.startTime);
+                objArray.dataValues.endTime = decrypt(objArray.dataValues.endTime);
+              }
+              
+              
+          }
+      }
+  },
     createdAt: false,
     updateAte: false,
     timestamps: false,
